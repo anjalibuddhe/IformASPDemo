d6f2e23aacd901252a39206ece2b61d8561a8de5
anjali buddhe <anjalibuddhe124@gmail.com>
2023-09-01 19:19:41 +0530

    Add project files.

---
+++ IformASPDemo/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using IformASPDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace IformASPDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<string> cities = new List<string>() { "Pune", "Nashik","Nagpur" ,"Mumbai"};
+            ViewData["cities"] = new SelectList(cities);
+            ViewData["list"] = new MultiSelectList(cities);
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Index(IFormCollection form,ICollection<string> hobbies)
+        {
+
+            ViewBag.Username = form["username"];
+            ViewBag.Gender = form["gender"];
+            ViewBag.City = form["cities"];
+            ViewBag.Hobbies = hobbies;
+            ViewBag.Comments = form["comments"];
+            return View("ShowInfo");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
